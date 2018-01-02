800ddf0b3b471915c90e401f3e3b780b2b3a6986
izaxveta <izavetha@gmail.com>
2018-01-02 11:28:35 -0700

    Experienced response anomalies in server, while response in tests worked. Responses now work in both test and server, view rendered, must refactor

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,7 @@ class SearchController < ApplicationController
   def index
     response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{params['q'].to_i},25))?format=json&show=storeId,storeType,longName,phone,city,distance&pageSize=10&apiKey=#{ENV['API_KEY']}")
     stores_data = JSON.parse(response.body)
+
     @stores = stores_data['stores'].map do |data|
       Store.new(data)
     end
