84224fa472d298ec440d045f4b660fbec24a2a73
izaxveta <izavetha@gmail.com>
2018-01-02 09:42:36 -0700

    Completed items api#destroy and create functionality and spec

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -5,4 +5,25 @@ class Api::V1::ItemsController < Api::V1::ApplicationController
     render status: 200, json: item, except: [:created_at, :updated_at]
   end
 
+  def create
+    item = Item.create(item_params)
+    render status: 201, json: {
+      item: item, except: [:created_at, :updated_at]
+    }
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    render json: {
+      message: "Successfully deleted item #{item.id}.",
+      status: 204
+    }
+  end
+
+  private
+
+    def item_params
+      params.require(:item).permit(:name, :description, :image_url)
+    end
+
 end
--- spec/requests/items_api_spec.rb
+++ spec/requests/items_api_spec.rb
@@ -1,24 +1,57 @@
 require 'rails_helper'
 
 RSpec.describe 'Items API' do
-  let(:item) { Item.create(name: "Fantastic Linen Shirt",
+  let(:item_1) { Item.create(name: "Fantastic Linen Shirt",
                 description: "Beatae doloremque praesentium. Et tempora est accusantium ut odio quis. Ad molestiae et sint rerum. Nisi et impedit rem sint et temporibus.",
-                image_url: "http://robohash.org/0.png?set=set2&bgset=bg1&size=200x200")
-            }
+                image_url: "http://robohash.org/0.png?set=set2&bgset=bg1&size=200x200")}
+
+
+  let(:item_2) { Item.create(name: "Fantastic Linen Skirt",
+                description: "Beatae doloremque praesentium. Et tempora est accusantium ut odio quis. Ad molestiae et sint rerum. Nisi et impedit rem sint et temporibus.",
+                image_url: "http://robohash.org/0.png?set=set2&bgset=bg1&size=200x200")}
 
   it 'can obtain a single item response' do
-    item
+    item_1
 
-    get "/api/v1/items/#{item.id}"
+    get "/api/v1/items/#{item_1.id}"
 
     content = JSON.parse(response.body)
 
     expect(response.status).to eq(200)
-    expect(content['id']).to eq(item.id)
-    expect(content['name']).to eq(item.name)
-    expect(content['description']).to eq(item.description)
-    expect(content['image_url']).to eq(item.image_url)
+    expect(content['id']).to eq(item_1.id)
+    expect(content['name']).to eq(item_1.name)
+    expect(content['description']).to eq(item_1.description)
+    expect(content['image_url']).to eq(item_1.image_url)
     expect(content['created_at']).to be_nil
     expect(content['updated_at']).to be_nil
   end
+
+  it 'can delete an item record response' do
+    item_1
+    item_2
+
+    delete "/api/v1/items/#{item_2.id}"
+
+    content = JSON.parse(response.body)
+
+    expect(content['status']).to eq(204)
+    expect(content['message']).to eq("Successfully deleted item #{item_2.id}.")
+  end
+
+  it 'can create an item record response' do
+    post '/api/v1/items', params: { item: { name: "Fantastic Linen Skirt",
+                                            description: "Beatae doloremque praesentium. Et tempora est accusantium ut odio quis. Ad molestiae et sint rerum. Nisi et impedit rem sint et temporibus.",
+                                            image_url: "http://robohash.org/0.png?set=set2&bgset=bg1&size=200x200"} }
+
+    content = JSON.parse(response.body)
+
+    expect(response.status).to eq(201)
+    expect(content['item']['name']).to eq("Fantastic Linen Skirt")
+    expect(Item.count).to eq(1)
+  end
+
+  # When I send a POST request to `/api/v1/items` with a name, description, and image_url
+  # I receive a 201 JSON  response if the record is successfully created
+  # And I receive a JSON response containing the id, name, description, and image_url but not the created_at or updated_at
+
 end
