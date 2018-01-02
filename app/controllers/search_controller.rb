8e0cde837666a8aed27f20191b6bbd4cee22525c
izaxveta <izavetha@gmail.com>
2018-01-02 11:18:32 -0700

    Api stores rendered to search index view

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,10 @@
 class SearchController < ApplicationController
   def index
-    response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{params['q']},25))?format=json&show=storeId,storeType,name&pageSize=10&apiKey=#{ENV['API_KEY']}")
+    response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{params['q'].to_i},25))?format=json&show=storeId,storeType,longName,phone,city,distance&pageSize=10&apiKey=#{ENV['API_KEY']}")
     stores_data = JSON.parse(response.body)
-require 'pry'; binding.pry
+    @stores = stores_data['stores'].map do |data|
+      Store.new(data)
+    end
+    @total = stores_data['total']
   end
-
 end
---
+++ app/models/store.rb
@@ -0,0 +1,16 @@
+class Store
+  attr_reader :name,
+              :type,
+              :phone,
+              :city,
+              :distance
+
+  def initialize(data)
+    @name = data['longName']
+    @type = data['storeType']
+    @phone = data['phone']
+    @city = data['city']
+    @distance = data['distance']
+  end
+
+end
