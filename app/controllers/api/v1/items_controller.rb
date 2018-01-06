565216e5721849fa4d7e120c7f5dcc0f279d2ddd
izaxveta <izavetha@gmail.com>
2018-01-05 22:30:14 -0700

    Refactored controller and added serialiers to make it less 'hacky'

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,29 +1,40 @@
 class Api::V1::ItemsController < Api::V1::ApplicationController
+  before_action :set_item, only: [:show, :destroy, :update]
+
+  def index
+    render status: 200, json: Item.all, each_serializer: ItemSerializer
+  end
 
   def show
-    item = Item.find(params[:id])
-    render status: 200, json: item, except: [:created_at, :updated_at]
+    render status: 200, json: item, serializer: ItemSerializer
   end
 
   def create
     item = Item.create(item_params)
-    render status: 201, json: item, except: [:created_at, :updated_at]
+    render status: 201, json: item, serializer: ItemSerializer
+  end
 
+  def update
+    item.update(item_params)
+    render status: 202, json: item, serializer: ItemSerializer
   end
 
   def destroy
-    item = Item.find(params[:id])
     item.destroy
     render json: {
       message: "Successfully deleted item #{item.id}.",
-      status: 204
+      status: 204    expect(content.first['created_at']).to be_nil
     }
   end
 
   private
+    attr_reader :item
+
+    def set_item
+      @item = Item.find(params[:id])
+    end
 
     def item_params
       params.require(:item).permit(:name, :description, :image_url)
     end
-
 end
