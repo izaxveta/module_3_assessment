c988c15a898be2a7f8bad27921fc367ffb576c1d
izaxveta <izavetha@gmail.com>
2018-01-08 13:36:31 -0700

    Refactored StorePresenter to make one store service request for each piece of data

--- app/presenters/store_presenter.rb
+++ app/presenters/store_presenter.rb
@@ -1,23 +1,28 @@
 class StorePresenter
   def initialize(filter = {})
     @filter = filter
+    @stores_data = get_stores_data
   end
 
   def stores
-    raw_data = store_service.get_stores_search(filter)['stores']
+    raw_data = stores_data['stores']
     generate_stores(raw_data)
   end
 
   def total
-    stores_data = store_service.get_stores_search(filter)['total']
+    stores_data['total']
   end
 
-  def generate_stores(stores_data)
-    stores_data.map { |data| Store.new(data) }
+  def generate_stores(raw_data)
+    raw_data.map { |data| Store.new(data) }
   end
 
   private
-    attr_reader :filter
+    attr_reader :filter, :stores_data
+
+    def get_stores_data
+      store_service.get_stores_search(filter)
+    end
 
     def store_service
       @store_service ||= StoreServices.new
